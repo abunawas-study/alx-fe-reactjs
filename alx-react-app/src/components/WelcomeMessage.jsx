1510899ed91564bd3e6e537e2a2c81fd6924d55c
Abusadiq Abunawas Nbanba <abunawasabusadiq@gmail.com>
2025-12-19 07:11:49 +0000

    fix a bug

--- alx-react-app/src/components/WelcomeMessage.jsx
+++ alx-react-app/src/components/WelcomeMessage.jsx
@@ -1,8 +1,8 @@
 function WelcomeMessage() {
     return (
         <div>
-            <h1>Hello everyone, I'm learning React at ALX!</h1>
-            <p>I'm learning about JSX components!</p>
+            <h1>"Hello everyone, I'm learning React at ALX!"</h1>
+            <p>"I'm learning about JSX components!"</p>
         </div>
     );
 }
