1717358bb9731af94e4232e8743b3a89007e1fee
Abusadiq Abunawas Nbanba <abunawasabusadiq@gmail.com>
2025-12-19 07:06:44 +0000

    moved Welcome Message files here

---
+++ alx-react-app/src/components/WelcomeMessage.jsx
@@ -0,0 +1,10 @@
+function WelcomeMessage() {
+    return (
+        <div>
+            <h1>Hello everyone, I'm learning React at ALX!</h1>
+            <p>I'm learning about JSX components!</p>
+        </div>
+    );
+}
+
+export default WelcomeMessage;
