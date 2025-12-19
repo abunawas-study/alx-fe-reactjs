bdb1eb6c631f800f84102c94fb6d21338c32a6c1
Abusadiq Abunawas Nbanba <abunawasabusadiq@gmail.com>
2025-12-19 06:09:17 +0000

    Welcome message file created

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import WelcomeMessage from './welcomeMessage.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -32,4 +33,5 @@ function App() {
   )
 }
 
+<WelcomeMessage />
 export default App
