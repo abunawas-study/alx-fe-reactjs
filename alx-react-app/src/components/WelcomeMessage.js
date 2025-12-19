14f1a58bc21f40be74e43b27e366ffc8fb437f91
Abusadiq Abunawas Nbanba <abunawasabusadiq@gmail.com>
2025-12-19 11:14:02 +0000

    fix a bug

--- alx-react-app/src/components/WelcomeMessage.js
+++ alx-react-app/src/components/WelcomeMessage.js
@@ -1,4 +1,4 @@
 import WelcomeMessage from './components/WelcomeMessage';
-import Header from './components/Header';
-import MainContent from './components/MainContent';
-import Footer from './components/Footer';
+import Header from './Header';
+import MainContent from './MainContent';
+import Footer from './Footer';
