62866e983a704863ce968120c19b1ddf26b685c2
Abusadiq Abunawas Nbanba <abunawasabusadiq@gmail.com>
2025-12-21 07:53:23 +0000

    User profile

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -21,6 +21,7 @@ function App() {
       </div>
       <Header />
       <MainContent />
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
       <Footer />
       <WelcomeMessage />
       
