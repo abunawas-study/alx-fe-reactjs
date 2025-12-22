afde54d12e5c09436ec9bfde7aab14fe83c4d248
Abusadiq Abunawas Nbanba <abunawasabusadiq@gmail.com>
2025-12-22 11:23:40 +0000

    updated the code

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -5,8 +5,8 @@ import './App.css'
 import WelcomeMessage from './components/WelcomeMessage';
 import Header from './components/Header';
 import MainContent from './components/MainContent';
-import UserProfile from './components/userProfile';
 import Footer from './components/Footer';
+import UserProfile from './components/UserProfile';
 function App() {
   const [count, setCount] = useState(0)
 
