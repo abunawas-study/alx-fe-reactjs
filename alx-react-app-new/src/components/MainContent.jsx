4998005b013f41505643ff064d860ef95e193b71
Abusadiq Abunawas Nbanba <abunawasabusadiq@gmail.com>
2025-12-28 09:09:09 +0000

    experiment inline styling in react application

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -1,6 +1,6 @@
 function MainContent() {
     return (
-        <main>
+        <main style={{padding: '20px', fontFamily: 'Arial, sans-serif'}}>
             <p>I love to visit New York, Paris, and Tokyo.</p>
         </main>
     )
